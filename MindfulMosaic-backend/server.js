f5d99d1a0548efcfe0b2b384721489eb541ebb8c
navneetyadavv <navneetyadav49710@gmail.com>
2024-08-29 09:52:37 +0530

    minor updates

--- MindfulMosaic-backend/server.js
+++ MindfulMosaic-backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import 'dotenv/config'
 import cors from 'cors'
-import admin from 'firebase-admin'
+import admin from 'firebase-admin';
 import { v2 as cloudinary } from 'cloudinary'
 import fileUpload from 'express-fileupload'
 import { ErrorThrow } from './utils/error.js'
@@ -12,6 +12,19 @@ import notificationRouter from './routes/notification.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  };
+  
 const server = express();
 server.use(express.json())
 server.use(fileUpload({
@@ -24,14 +37,13 @@ server.use(cors())
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.resolve(__dirname, 'dist')));
+server.use(express.static(path.resolve(__dirname, 'dist')));
 
 
 
-const serviceAccountKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountKey)
-})
+    credential: admin.credential.cert(serviceAccount),
+  });
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
